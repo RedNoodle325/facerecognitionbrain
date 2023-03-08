83d562c33156e2a0ddab772913cfb33a28ee25c0
Zak Klinedinst <zak.klinedinst@proton.me>
2023-03-07 22:35:40 -0500

    background and components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ParticlesBg type='circle' bg={true} />
         <Navigation />
         <Logo />
-        {/*<ImageLinkForm />
-    <FaceRecognition />*/}
+        <Rank />
+        <ImageLinkForm />
+    {/*<FaceRecognition />*/}
       </div>
     );
   }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
+import './Logo.css';
+import face from './face.png';
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-            <Tilt className='Tilt br2 shadow-2' options={{ max: 25 }} style={{ height: 250, width: 250 }}>
-                <img alt='RedNoodles' src='https://i.imgur.com/unnJD0X.png'></img>
+            <Tilt className='Tilt br2 shadow-2' options={{ max: 25 }} style={{ height: 150, width: 150 }}>
+            <div className="Tilt-inner"><img style={{paddingTop:'22px'}}alt='face' src={face}/></div>
             </Tilt>
         </div>
     );
