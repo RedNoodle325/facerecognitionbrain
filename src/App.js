3fc478e43e82c9689467a508cb5c7349bbc3abed
Zak Klinedinst <zak.klinedinst@proton.me>
2023-03-08 20:45:36 -0500

    face

--- src/App.js
+++ src/App.js
@@ -5,20 +5,56 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+ apiKey: '6f2d36b91be645dbb08b8779f4336ad5'
+});
+
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+
+    }
+  }
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict(
+      {
+        id: 'face-detection',
+        name: 'face-detection',
+        version: '6dc7e46bc9124c5c8824be4822abe105',
+        type: 'visual-detector',
+      }, this.state.input)
+    }
+
   render() {
+    const {imageUrl} = this.state
     return (
       <div className="App">
         <ParticlesBg type='circle' bg={true} />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-    {/*<FaceRecognition />*/}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition imageUrl={imageUrl}/>
       </div>
     );
   }
 }
+
 
 export default App;
